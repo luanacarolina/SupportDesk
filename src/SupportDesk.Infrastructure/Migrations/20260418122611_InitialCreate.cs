e7b2d9a3557bdea2ac44bfe8d1d19dca5b542f1f
Luana Carolina <luanalcj1@hotmail.com>
2026-04-18 09:50:43 -0300

    feat: adiciona migration inicial do banco de dados

---
+++ src/SupportDesk.Infrastructure/Migrations/20260418122611_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SupportDesk.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "support_tickets",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ClientName = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    ProblemDescription = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false),
+                    Priority = table.Column<int>(type: "integer", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_support_tickets", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "support_tickets");
+        }
+    }
+}
